959f91a0abda4d2cdcd4753c843680b5a7baf193
unknown <127o1404@itsx.edu.mx>
2022-05-26 12:22:54 -0500

    Ejercicios JavaScript Fundamentos

---
+++ js/main.js
@@ -0,0 +1,39 @@
+let x = 3;
+console.log(x)
+console.log("console log desde mainJS");
+
+
+document.getElementById("encabezado").innerHTML=("Fundamentos " + x);
+
+//String
+let nombre = "Bruno";
+console.log(typeof(nombre));
+
+//Number 
+let edad = 25;
+console.log(typeof(edad));
+
+//Boolean
+let chilaquieres_rojos = true;
+console.log(typeof(chilaquieres_rojos));
+
+//undefined
+let equipoFut;
+console.log(typeof(equipoFut));
+
+//null
+let graduado = null;
+console.log(typeof(graduado));
+
+//Object
+let calificaciones = {};
+console.log(typeof(calificaciones));
+
+edad = parseInt(edad) + 1;
+console.log(edad);
+
+console.log(typeof(edad));
+
+console.log(parseInt(nombre));
+
+console.log("20"  * 2);
